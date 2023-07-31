6969db459418d910fe9a607970b30edac83d1c44
sledge79 <tonywalsh1511@gmail.com>
2023-07-31 17:33:54 +0000

    Part 2 commit
    Signed-off-by: sledge79 <tonywalsh1511@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 // Why this occured: CO_E_WRONG_SERVER_IDENTITY
-
+// printf statement both in the interrupt context & main loop (Part 2 error)
 
 #include "mbed.h"
 
 InterruptIn button(PC_13); 
+volatile bool buttonPressedFlag = false;
+
 
 void button_interrupt() {
+    buttonPressedFlag = true;
     printf("Button pressed!\r\n");
 }
 
@@ -14,7 +17,13 @@ int main() {
 
 
     while (true) {
-
+        if (buttonPressedFlag) {
+            printf("Button pressed flag set!\r\n"); 
+            buttonPressedFlag = false; 
+        }
     }
 }
+
+    
+
 
